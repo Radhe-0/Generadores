87ebe70a50c735a989c4936d5af505c4264103bd
Radhe <30527877+Radhe-0@users.noreply.github.com>
2022-09-26 10:28:55 -0500

    ADD: DatosSVM23D

--- Generadores.py
+++ Generadores.py
@@ -165,6 +165,80 @@ def DatosSVM2(num=10):
         lista.append(i)
 
     return lista
+#################################################
+
+def DatosSVM23D():
+    
+    lista = []
+    
+    X = np.linspace(0,100,num)
+    Y = np.linspace(0,100,num)
+    Z = np.linspace(0,100,num)
+
+    Xmod = X * np.random.randn(num)
+    Ymod = Y * np.random.randn(num) + 700
+    Zmod = Z * np.random.randn(num)
+
+
+    Xmod2 = X * np.random.randn(num) + 500
+    Ymod2 = Y * np.random.randn(num) + 700
+    Zmod2 = Z * np.random.randn(num)
+
+    Xmod3 = X * np.random.randn(num)
+    Ymod3 = Y * np.random.randn(num) + 700
+    Zmod3 = Z * np.random.randn(num) + 500
+
+    Xmod4 = X * np.random.randn(num) + 500
+    Ymod4 = Y * np.random.randn(num) + 700
+    Zmod4 = Z * np.random.randn(num) + 500
+
+
+    for i in zip(Xmod, Ymod, Zmod):
+        lista.append(i)
+
+    for i in zip(Xmod2, Ymod2, Zmod2):
+        lista.append(i)
+
+    for i in zip(Xmod3, Ymod3, Zmod3):
+        lista.append(i)
+
+    for i in zip(Xmod4, Ymod4, Zmod4):
+        lista.append(i)
+
+    X = np.linspace(0,100,num)
+    Y = np.linspace(0,100,num)
+    Z = np.linspace(0,100,num)
+
+    Xmod5 = X * np.random.randn(num) - 500
+    Ymod5 = Y * np.random.randn(num)
+    Zmod5 = Z * np.random.randn(num) - 500
+
+
+    Xmod6 = X * np.random.randn(num)
+    Ymod6 = Y * np.random.randn(num)
+    Zmod6 = Z * np.random.randn(num) - 500
+
+    Xmod7 = X * np.random.randn(num) - 500
+    Ymod7 = Y * np.random.randn(num)
+    Zmod7 = Z * np.random.randn(num)
+
+    Xmod8 = X * np.random.randn(num)
+    Ymod8 = Y * np.random.randn(num)
+    Zmod8 = Z * np.random.randn(num)
+
+    for i in zip(Xmod5, Ymod5, Zmod5):
+        lista.append(i)
+
+    for i in zip(Xmod6, Ymod6, Zmod6):
+        lista.append(i)
+
+    for i in zip(Xmod7, Ymod7, Zmod7):
+        lista.append(i)
+
+    for i in zip(Xmod8, Ymod8, Zmod8):
+        lista.append(i)
+
+    return lista
 
 #################################################
 
